82b5a2519228a2f0461455afc2ab304136abc077
Dongximing <dongx34@myumanitoba.ca>
2024-10-08 16:28:17 -0400

    add length  to 1024

--- hg_infer.py
+++ hg_infer.py
@@ -73,7 +73,7 @@ def generate_text(model, tokenizer,input):
     model_input = tokenizer(input, return_tensors="pt", padding=True, truncation=True).to("cuda")
     model.eval()
     with torch.no_grad():
-        output_ids = model.generate(model_input["input_ids"], max_new_tokens=2, temperature=0.01,
+        output_ids = model.generate(model_input["input_ids"], max_new_tokens=30, temperature=0.01,
                                     return_dict_in_generate=True, output_scores=True)
         input_length = 1 if model.config.is_encoder_decoder else model_input.input_ids.shape[1]
         generated_tokens = output_ids.sequences[:, input_length:]
